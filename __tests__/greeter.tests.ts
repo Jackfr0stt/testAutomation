bfae401e486f8a25f74d3e3daa783af040798d41
Jackfr0stt <57899488+Jackfr0stt@users.noreply.github.com>
2022-04-15 09:51:09 +0100

    Updates files for tests

--- __tests__/greeter.tests.ts
+++ __tests__/greeter.tests.ts
@@ -1,5 +1,9 @@
-import { Greeter } from '../src/index';
+import { Greeter, cleaner } from '../src/index';
 
 test('My Greeter', () => {
   expect(Greeter('Carl')).toBe('Hello Carl');
+});
+
+test('Cleaning files', () => {
+  expect(cleaner()).toBe('Everything is sparkling.');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import fs = require('fs');
+
 export const Greeter = (name: string) => `Hello ${name}`;
+
+export const cleaner = () => {
+  fs.rmSync('../test', { recursive: true, force: true });
+
+  return `Everything is sparkling.`;
+}
